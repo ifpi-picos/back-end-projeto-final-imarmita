27815cafef6178dfda03fde134feeba5e6c6a308
Luis Gustavo <luisgustavo.ifpi@gmail.com>
2020-01-27 15:17:38 -0300

    alterações

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,11 +1,13 @@
 class Config {
   constructor() {
     this.env = 'production';
-    this.PORT = 3000;
+    this.PORT = process.env.PORT || 3000;
     this.API_BASE = '/api';
     const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
     const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : '27017';
     this.MONGODB_URL = `mongodb://${DB_HOST}':'${DB_PORT}/db_prod`;
+    this.MONGO_SERVER = "mongodb://u2jktbnyjtohhofyl3y1:YqECz0KkWTYJ8EApLa1H@bnuwdrer8ngqlf4-mongodb.services.clever-cloud.com:27017/bnuwdrer8ngqlf4";
+
   }
 }
 
